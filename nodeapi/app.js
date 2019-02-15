d090edc99bf220bd59a6d701d03de5e4bc409581
Jeremiah Ogutu <jogutu@pm.me>
2019-02-15 18:27:49 -0500

    separating routes to a routes folder

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world from node js")
-});
+// routes
+const { getPosts } = require('./routes/post');
+app.get("/", getPosts);
 
 const port = 8080;
 

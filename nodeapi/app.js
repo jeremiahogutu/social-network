4bc003800beabc2ed30b3948ae376a7e755fa708
Jeremiah Ogutu <jogutu@pm.me>
2019-02-15 18:53:09 -0500

    controller actions

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -3,12 +3,12 @@ const app = express();
 const morgan = require("morgan");
 
 // routes
-const { getPosts } = require('./routes/post');
+const postRoutes = require('./routes/post');
 
 //middleware
 app.use(morgan("dev"));
 
-app.get("/", getPosts);
+app.use("/", postRoutes);
 
 const port = 8080;
 app.listen(port, () => {

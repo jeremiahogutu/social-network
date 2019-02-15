b27a76e1d23b5e5c13499c456a3571ca891ee41b
Jeremiah Ogutu <jogutu@pm.me>
2019-02-15 18:42:09 -0500

    added morgan middleware

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 
 // routes
 const { getPosts } = require('./routes/post');
+
+//middleware
+app.use(morgan("dev"));
+
 app.get("/", getPosts);
 
 const port = 8080;
-
 app.listen(port, () => {
     console.log(`Node Api is listening on port ${port}`)
 });

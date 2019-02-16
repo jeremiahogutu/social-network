2327a857437e9b0034b31a0d23be08eff400033e
Jeremiah Ogutu <jogutu@pm.me>
2019-02-15 19:18:18 -0500

    connected database using mongoose

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
-const morgan = require("morgan");
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
 
+// db
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+    .then(() => console.log('DB connected'));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+});
 // routes
 const postRoutes = require('./routes/post');
 
@@ -10,7 +20,7 @@ app.use(morgan("dev"));
 
 app.use("/", postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Node Api is listening on port ${port}`)
 });

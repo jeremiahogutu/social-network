8d5fe20a98496ae088c45c1f04e40d09ba8a2641
Jeremiah Ogutu <jogutu@pm.me>
2019-02-18 08:14:35 -0500

    finding user by id

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { createPost, getPosts } = require('../controllers/post');
 const { requireSignin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
 const { createPostValidator }  = require('../validator');
 const router = express.Router();
 
 router.get('/', requireSignin, getPosts);
 router.post('/post', createPostValidator, createPost);
+
+// any route containing :userId, our app will first execute userById() method
+router.param("userId", userById);
 
 module.exports = router;

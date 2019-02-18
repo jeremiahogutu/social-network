efec4493119b5f0ddc5704916be55d2f08aead99
Jeremiah Ogutu <jogutu@pm.me>
2019-02-17 22:30:20 -0500

    signup validation

--- nodeapi/validator/index.js
+++ nodeapi/validator/index.js
@@ -15,11 +15,44 @@ exports.createPostValidator = (req, res, next) => {
     const errors = req.validationErrors();
 
     // Show errors as they happen and limit to the first one
-    if(errors) {
+    if (errors) {
         const firstError = errors.map((error) => error.msg)[0];
         return res.status(400).json({error: firstError})
     }
 
     // proceed to next middleware
     next();
+};
+
+
+exports.userSignupValidator = (req, res, next) => {
+    // name is not null and between 4-10 characters
+    req.check('name', 'Name is required').notEmpty();
+    // email is not null, valid and normalized
+    req.check('email', 'Email must be between 3 to 32 characters')
+        .matches(/.+@.+\..+/)
+        .withMessage('Email must contain @')
+        .isLength({
+            min: 4,
+            max: 2000
+        });
+    // check for password
+    req.check('name', 'Name is required').notEmpty();
+    req.check('password')
+        .isLength({min: 8})
+        .withMessage('Password must contain at least 8 characters')
+        .matches(/\d/)
+        .withMessage('Password must contain a number');
+    // check errors
+    const errors = req.validationErrors();
+
+    // Show errors as they happen and limit to the first one
+    if (errors) {
+        const firstError = errors.map((error) => error.msg)[0];
+        return res.status(400).json({error: firstError})
+    }
+
+    // proceed to next middleware
+    next();
+
 };

6158ba3e6cce8fa33d4ed7eda459fd7abad4d00e
Jeremiah Ogutu <jogutu@pm.me>
2019-02-18 07:50:25 -0500

    protected routes

--- nodeapi/controllers/auth.js
+++ nodeapi/controllers/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const expressJwt = require('express-jwt');
 const User = require('../models/user');
 
 exports.signup = async (req, res) => {
@@ -45,3 +46,7 @@ exports.signout = (req, res) => {
     res.clearCookie("t");
     return res.json({message: "Signout success"})
 };
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET
+});
--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createPost, getPosts } = require('../controllers/post');
+const { requireSignin } = require('../controllers/auth');
 const { createPostValidator }  = require('../validator');
 const router = express.Router();
 
-router.get('/', getPosts);
+router.get('/', requireSignin, getPosts);
 router.post('/post', createPostValidator, createPost);
 
 module.exports = router;

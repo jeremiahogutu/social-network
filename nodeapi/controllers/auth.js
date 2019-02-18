f48a3e9224867ff3af87c3d092708bbabd6dcec8
Jeremiah Ogutu <jogutu@pm.me>
2019-02-17 22:57:52 -0500

    added cookie parser and jsonwebtoken

--- nodeapi/controllers/auth.js
+++ nodeapi/controllers/auth.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const User = require('../models/user');
 
 exports.signup = async (req, res) => {
@@ -10,4 +12,19 @@ exports.signup = async (req, res) => {
     const user = await new User(req.body);
     await user.save();
     res.status(200).json({ message: "Signup success! Please login." })
+};
+
+exports.signin = (req, res) => {
+    // find the user based on email
+    const {_id, name, email, password} = req.body
+
+    // if error or no user
+
+    // if user, authenticate
+
+    // generate a token with user id and secret
+
+    // persist the token as 't' in cookie with expiry date
+
+    // retrun response with user and token to frontend client
 };

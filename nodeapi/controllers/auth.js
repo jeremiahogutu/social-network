4bfb210715b5a307706c0bbd263071ebb3e9dc63
Jeremiah Ogutu <jogutu@pm.me>
2019-02-17 23:48:23 -0500

    user sign in with jwt

--- nodeapi/controllers/auth.js
+++ nodeapi/controllers/auth.js
@@ -16,15 +16,27 @@ exports.signup = async (req, res) => {
 
 exports.signin = (req, res) => {
     // find the user based on email
-    const {_id, name, email, password} = req.body
-
-    // if error or no user
-
-    // if user, authenticate
-
-    // generate a token with user id and secret
-
-    // persist the token as 't' in cookie with expiry date
-
-    // retrun response with user and token to frontend client
+    const {email, password} = req.body;
+    User.findOne({email}, (err, user) => {
+        // if err or no user
+        if (err || !user) {
+            return res.status(401).json({
+                error: 'User with that email does not exist. Please signin'
+            })
+        }
+        // if user is found make sure the email and password match
+        // create authenticate method in model and use here
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email and password do not match'
+            })
+        }
+        // generate a token with user id and secret
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+        // persist the token as 't' in cookie with expiry date
+        res.cookie("t", token, {expire: new Date() + 9999});
+        // return response with user and token to frontend client
+        const {_id, name, email} = user;
+        return res.json({token, user: {_id, email, name}})
+    })
 };

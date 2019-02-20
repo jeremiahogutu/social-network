78f971b7e6557dc727905b83f84f548656c5b0b1
Jeremiah Ogutu <jogutu@pm.me>
2019-02-20 09:21:37 -0500

    sanitized data and changed order in which the functions run when creating a new post

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -22,6 +22,8 @@ exports.createPost = (req, res) => {
             })
         }
         let post = new Post(fields);
+        req.profile.hashed_password = undefined;
+        req.profile.salt = undefined;
         post.postedBy = req.profile;
         if (files.photo) {
             post.photo.data = fs.readFileSync(files.photo.path);
--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -6,7 +6,12 @@ const { createPostValidator }  = require('../validator');
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/post', requireSignin, createPostValidator, createPost);
+router.post(
+    '/post/new/:userId',
+    requireSignin,
+    createPost,
+    createPostValidator
+);
 
 // any route containing :userId, our app will first execute userById() method
 router.param("userId", userById);

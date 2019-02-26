b7a9596b9b7f9f434cbc634594e017fb128834e0
Jeremiah Ogutu <jogutu@pm.me>
2019-02-26 07:41:34 -0500

    added signup route and component

--- social-front-end/src/MainRouter.js
+++ social-front-end/src/MainRouter.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from "./components/Home";
+import Signup from "./components/user/Signup";
 
 const MainRouter = () => (
     <div>
-        <switch>
-            <Route path="/" component={Home} />
-        </switch>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup}/>
+        </Switch>
     </div>
 );
 

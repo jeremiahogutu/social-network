68c48f12cea703403569ca9c9551f99ed238e5df
Jeremiah Ogutu <jogutu@pm.me>
2019-02-26 08:10:25 -0500

    signup init

--- social-front-end/src/components/user/Signup.js
+++ social-front-end/src/components/user/Signup.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
 
 class Signup extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            error: ''
+        }
+    }
     render() {
         return (
-            <div>
-                <h2>Signup</h2>
+            <div className='ui container'>
+                <h2 className='ui header'>Signup</h2>
+                <form className="ui form">
+                    <div className="field">
+                        <label>First Name</label>
+                        <input type="text" name="first-name" placeholder="First Name"/>
+                    </div>
+                    <div className="field">
+                        <label>Last Name</label>
+                        <input type="text" name="last-name" placeholder="Last Name"/>
+                    </div>
+                    <div className="field">
+                        <div className="ui checkbox">
+                            <input type="checkbox" tabIndex="0" className="hidden"/>
+                                <label>I agree to the Terms and Conditions</label>
+                        </div>
+                    </div>
+                    <button className="ui button" type="submit">Submit</button>
+                </form>
             </div>
         );
     }

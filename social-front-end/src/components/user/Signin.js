8ef93f3de54dea5cacdc02d156fe18ad37fc48aa
Jeremiah Ogutu <jogutu@pm.me>
2019-03-04 09:51:42 -0500

    login form init still needs work

--- social-front-end/src/components/user/Signin.js
+++ social-front-end/src/components/user/Signin.js
@@ -65,29 +65,55 @@ class Signin extends Component {
     };
 
     signInForm = (email, password) => (
-        <form className="ui form">
-            <div className="field">
-                <label>Email</label>
-                <input
-                    onChange={this.handleChange("email")}
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={email}
-                />
+        <div className="mdl-grid">
+            <div className="mdl-card mdl-shadow--16dp util-center util-spacing-h--40px">
+                <div className="mdl-card__title mdl-color--orange-800">
+                    <h2 className="mdl-card__title-text mdl-color-text--white">User Login</h2>
+                </div>
+                <div className="mdl-card__supporting-text mdl-grid">
+                    <b className="mdl-color-text--accent">Error message goes here</b>
+                    <form>
+                        <div
+                            className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-cell mdl-cell--12-col">
+                            <label className="mdl-textfield__label mdl-color-text--grey"
+                                   htmlFor="textfield_username">Email</label>
+                            <input
+                                onChange={this.handleChange("email")}
+                                className="mdl-textfield__input"
+                                type="email"
+                                id="textfield_username"
+                                name="email"
+                                placeholder="Email"
+                                value={email}
+                            />
+                        </div>
+                        <div
+                            className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-cell mdl-cell--12-col">
+                            <label className="mdl-textfield__label mdl-color-text--grey"
+                                   htmlFor="textfield_password">Password</label>
+                            <input
+                                className="mdl-textfield__input"
+                                onChange={this.handleChange("password")}
+                                type="password"
+                                id="textfield_password"
+                                name="password"
+                                placeholder="password"
+                                value={password}
+                            />
+                        </div>
+                        <div className="mdl-cell mdl-cell--12-col send-button" align="center">
+                            <button
+                                onClick={this.onSubmit}
+                                type="submit"
+                                className="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--raised mdl-button--colored mdl-color--primary"
+                            >
+                                Log In
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="field">
-                <label>Password</label>
-                <input
-                    onChange={this.handleChange("password")}
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    value={password}
-                />
-            </div>
-            <button onClick={this.onSubmit} className="ui button" type="submit">Submit</button>
-        </form>
+        </div>
     );
 
     render() {
@@ -105,12 +131,10 @@ class Signin extends Component {
                     loading ? (
                         <div className="ui message">
                             <h2>loading...</h2>
-                        </div>) : (""
-                    )
+                        </div>) : ("")
                 }
             </div>
-    )
-        ;
+    );
     }
 }
 

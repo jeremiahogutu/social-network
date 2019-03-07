fbe584a05598a7966ab81654ef1b36f425f35e30
Jeremiah Ogutu <jogutu@pm.me>
2019-03-07 07:26:16 -0500

    conditional rendering for signin, signout and logout links

--- social-front-end/src/App.js
+++ social-front-end/src/App.js
@@ -13,7 +13,17 @@ export const signout = (next) => {
     }).catch(err => console.log(err))
 };
 
+export const isAuthenticated = () => {
+    if (typeof window == "undefined") {
+        return false
+    }
 
+    if(localStorage.getItem("jwt")) {
+        return JSON.parse(localStorage.getItem("jwt"))
+    } else {
+        return false
+    }
+};
 
 const App = () => (
     <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -26,12 +36,19 @@ const App = () => (
                 {/*Navigation. We hide it in small screens.*/}
                 <nav className="mdl-navigation mdl-layout--large-screen-only">
                     <NavLink className="mdl-navigation__link" to="/" activeStyle={{fontWeight: "bold", color: "red"}} exact={true}>Home</NavLink>
-                    <NavLink className="mdl-navigation__link" to="/signin" activeStyle={{fontWeight: "bold", color: "red"}}>Sign In</NavLink>
-                    <NavLink className="mdl-navigation__link" to="/signup" activeStyle={{fontWeight: "bold", color: "red"}}>Sign Up</NavLink>
-                    <button
-                        className="mdl-navigation__link"
-                        style={{cursor: "pointer", color: "#fff", background: "transparent", border: "none"}}
-                    onClick={() => signout(() => window.location.href = "http://localhost:3000/")}>Sign Out</button>
+
+                    {!isAuthenticated() &&(
+                        <>
+                            <NavLink className="mdl-navigation__link" to="/signin" activeStyle={{fontWeight: "bold", color: "red"}}>Sign In</NavLink>
+                            <NavLink className="mdl-navigation__link" to="/signup" activeStyle={{fontWeight: "bold", color: "red"}}>Sign Up</NavLink>
+                        </>
+                    )}
+                    {isAuthenticated() && (
+                        <button
+                            className="mdl-navigation__link"
+                            style={{cursor: "pointer", color: "#fff", background: "transparent", border: "none"}}
+                            onClick={() => signout(() => window.location.href = "http://localhost:3000/")}>Sign Out</button>
+                    )}
                 </nav>
             </div>
         </header>

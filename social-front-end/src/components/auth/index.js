2e95807bfbc53211cf05827e920824f7ef7f88d6
Jeremiah Ogutu <jogutu@pm.me>
2019-05-21 23:36:44 -0400

    code refactor

---
+++ social-front-end/src/components/auth/index.js
@@ -0,0 +1,36 @@
+export const signup = user => {
+    return fetch("http://localhost:3005/signup", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err))
+};
+
+export const signin = user => {
+    return fetch("http://localhost:3005/signin", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err))
+};
+
+export const authenticate = (jwt, next) => {
+    if (typeof window !== "undefined") {
+        localStorage.setItem("jwt", JSON.stringify(jwt));
+        next()
+    }
+};

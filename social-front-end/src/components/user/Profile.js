098df132baa5c3fff9bb5034182b4b0aec20f5b2
Jeremiah Ogutu <jogutu@pm.me>
2019-05-22 00:07:51 -0400

    user info from local storage

--- social-front-end/src/components/user/Profile.js
+++ social-front-end/src/components/user/Profile.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
+import {isAuthenticated} from "../../App";
 
 class Profile extends Component {
+    constructor() {
+        super();
+        this.state = {
+            user: "",
+            redirectToSignin: false
+        }
+    }
+
+    componentDidMount() {
+        console.log("user id from route params: ", this.props.match.params.userId)
+    }
+
     render() {
         return (
             <div className='container'>
                 <h2>Profile</h2>
+                <p>Hello {isAuthenticated().user.name}</p>
+                <p>Email: {isAuthenticated().user.email}</p>
             </div>
         );
     }

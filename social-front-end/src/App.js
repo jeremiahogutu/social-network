d36308156a0fe59c4d841296811104b108e02ca2
Jeremiah Ogutu <jogutu@pm.me>
2019-05-22 23:12:19 -0400

    fetch user profile

--- social-front-end/src/App.js
+++ social-front-end/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import MainRouter from "./MainRouter";
-import {isAuthenticated, signout} from "./components/auth";
+import {close, isAuthenticated, signout} from "./components/auth";
+
 
 const App = () => (
     <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -9,43 +10,57 @@ const App = () => (
             <div className="mdl-layout__header-row">
                 {/*Title*/}
                 <NavLink style={{textDecoration: "none", color: "#fff"}} to={`/user/${isAuthenticated().user._id}`}>
-                <span className="mdl-layout-title">{isAuthenticated() ? `${isAuthenticated().user.name}'s profile` : "Social Network"}</span>
+                    <span
+                        className="mdl-layout-title">{isAuthenticated() ? `${isAuthenticated().user.name}'s profile` : "Social Network"}</span>
                 </NavLink>
                 {/*Add spacer, to align navigation to the right */}
                 <div className="mdl-layout-spacer"/>
                 {/*Navigation. We hide it in small screens.*/}
                 <nav className="mdl-navigation mdl-layout--large-screen-only">
-                    <NavLink className="mdl-navigation__link" to="/" activeStyle={{fontWeight: "bold", color: "red"}} exact={true}>Home</NavLink>
+                    <NavLink className="mdl-navigation__link" to="/" activeStyle={{fontWeight: "bold", color: "red"}}
+                             exact={true}>Home</NavLink>
 
-                    {!isAuthenticated() &&(
+                    {!isAuthenticated() && (
                         <>
-                            <NavLink className="mdl-navigation__link" to="/signin" activeStyle={{fontWeight: "bold", color: "red"}}>Sign In</NavLink>
-                            <NavLink className="mdl-navigation__link" to="/signup" activeStyle={{fontWeight: "bold", color: "red"}}>Sign Up</NavLink>
+                            <NavLink className="mdl-navigation__link" to="/signin"
+                                     activeStyle={{fontWeight: "bold", color: "red"}}>Sign In</NavLink>
+                            <NavLink className="mdl-navigation__link" to="/signup"
+                                     activeStyle={{fontWeight: "bold", color: "red"}}>Sign Up</NavLink>
                         </>
                     )}
                     {isAuthenticated() && (
                         <button
                             className="mdl-navigation__link"
                             style={{cursor: "pointer", color: "#fff", background: "transparent", border: "none"}}
-                            onClick={() => signout(() => window.location.href = "http://localhost:3000/")}>Sign Out</button>
+                            onClick={() => signout(() => window.location.href = "http://localhost:3000/")}>Sign
+                            Out</button>
                     )}
                 </nav>
             </div>
         </header>
-        <div className="mdl-layout__drawer">
+        <div className="mdl-layout__drawer" onClick={close}>
             <span className="mdl-layout-title">Social Network</span>
             <nav className="mdl-navigation">
-                <NavLink className="mdl-navigation__link" to="/" activeStyle={{fontWeight: "bold", color: "red"}} exact={true}>Home</NavLink>
-                {!isAuthenticated() &&(
+                <NavLink className="mdl-navigation__link" to="/" activeStyle={{fontWeight: "bold", color: "red"}}
+                         exact={true}>Home</NavLink>
+                {!isAuthenticated() && (
                     <>
-                        <NavLink className="mdl-navigation__link" to="/signin" activeStyle={{fontWeight: "bold", color: "red"}}>Sign In</NavLink>
-                        <NavLink className="mdl-navigation__link" to="/signup" activeStyle={{fontWeight: "bold", color: "red"}}>Sign Up</NavLink>
+                        <NavLink className="mdl-navigation__link" to="/signin"
+                                 activeStyle={{fontWeight: "bold", color: "red"}}>Sign In</NavLink>
+                        <NavLink className="mdl-navigation__link" to="/signup"
+                                 activeStyle={{fontWeight: "bold", color: "red"}}>Sign Up</NavLink>
                     </>
                 )}
                 {isAuthenticated() && (
                     <button
                         className="mdl-navigation__link"
-                        style={{cursor: "pointer", color: "#757575", background: "transparent", border: "none", textAlign: "left"}}
+                        style={{
+                            cursor: "pointer",
+                            color: "#757575",
+                            background: "transparent",
+                            border: "none",
+                            textAlign: "left"
+                        }}
                         onClick={() => signout(() => window.location.href = "http://localhost:3000/")}>Sign Out</button>
                 )}
             </nav>
@@ -55,5 +70,4 @@ const App = () => (
         </main>
     </div>
 );
-
 export default App;

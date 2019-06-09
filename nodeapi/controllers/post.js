3e344a04901c9d6dd5de0c393f3cec35391ea0b6
Jeremiah Ogutu <jogutu@pm.me>
2019-06-08 23:09:02 -0400

    post image

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -108,3 +108,8 @@ exports.deletePost = (req, res) => {
         })
     })
 };
+
+exports.photo = (req, res, next) => {
+    res.set("Content-Type", req.post.photo.contentType)
+    return res.send(req.post.photo.data)
+}
